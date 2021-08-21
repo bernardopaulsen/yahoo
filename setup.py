438bc198873bca6a782a3b85143d42c8350a7465
Bernardo Paulsen <bernardohpaulsen@gmail.com>
2021-08-21 18:29:23 -0300

    Now setup.py reads requirements from requirements.txt.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-with open("requirements_dev.txt", "r") as requirements_file:
+with open("requirements.txt", "r") as requirements_file:
     requirements = [line.rstrip() for line in requirements_file]
 
 setup(

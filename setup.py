0c72a7776d2bdd5d4a0d9628f4552f5401d4aff9
Bernardo Paulsen <bernardohpaulsen@gmail.com>
2021-08-21 19:19:47 -0300

    First successful building of package.

--- setup.py
+++ setup.py
@@ -3,23 +3,25 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
-
-with open("requirements.txt", "r") as requirements_file:
-    requirements = [line.rstrip() for line in requirements_file]
-
-setup(
-    name="yahoo",
-    version="0.1.0",
-    author="Bernardo Paulsen",
-    author_email="bernardopaulsen@gmail.com",
-    description="A package to import data from Yahoo Finance! API.",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/bernardopaulsen/yahoo",
-    packages=find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
-        ],
-)
+    requirements = [
+        "datetime>=4.3",
+        "numpy>=1.21.1",
+        "pandas>=1.3.0",
+        "pandas-datareader>=0.10.0"
+        ]
+    setup(
+        name="yahoo",
+        version="0.1.0",
+        author_email="bernardopaulsen@gmail.com",
+        author="Bernardo Paulsen",
+        description="A package to import data from Yahoo Finance! API.",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        url="https://github.com/bernardopaulsen/yahoo",
+        packages=find_packages(),
+        install_requires=requirements,
+        classifiers=[
+            "Programming Language :: Python :: 3.9",
+            "License :: OSI Approved :: MIT License",
+            ],
+        )
